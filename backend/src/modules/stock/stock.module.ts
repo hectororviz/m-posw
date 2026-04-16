ca23a709f13e076aa0e9308271f662c185fb937b
Ubuntu <ubuntu@vps-3051a159.vps.ovh.ca>
2026-04-16 11:13:48 +0000

    fix: agregar PrismaService a providers de StockModule

--- backend/src/modules/stock/stock.module.ts
+++ backend/src/modules/stock/stock.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../common/prisma.service';
 import { StockService } from './stock.service';
 import { StockController } from './stock.controller';
 
 @Module({
   controllers: [StockController],
-  providers: [StockService],
+  providers: [StockService, PrismaService],
 })
 export class StockModule {}
