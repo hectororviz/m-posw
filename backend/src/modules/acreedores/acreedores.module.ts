0bbdb58ce24e50b6951afa7f0f8ccf977604c512
Ubuntu <ubuntu@vps-3051a159.vps.ovh.ca>
2026-06-24 17:06:35 +0000

    fix: import UsersModule in AcreedoresModule for ModuleAccessGuard

--- backend/src/modules/acreedores/acreedores.module.ts
+++ backend/src/modules/acreedores/acreedores.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../common/prisma.service';
 import { TreasuryModule } from '../treasury/treasury.module';
+import { UsersModule } from '../users/users.module';
 import { AcreedoresController } from './acreedores.controller';
 import { AcreedoresService } from './acreedores.service';
 
 @Module({
-  imports: [TreasuryModule],
+  imports: [TreasuryModule, UsersModule],
   controllers: [AcreedoresController],
   providers: [AcreedoresService, PrismaService],
   exports: [AcreedoresService],
