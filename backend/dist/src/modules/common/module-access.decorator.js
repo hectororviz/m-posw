3fe47bd31dfc03765723c2a0c4ab117d099dce76
Ubuntu <ubuntu@vps-3051a159.vps.ovh.ca>
2026-06-29 13:59:56 +0000

    fix: agregar dist/ a gitignore y eliminar archivos commiteados por error

--- backend/dist/src/modules/common/module-access.decorator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RequireModule = exports.MODULE_ACCESS_KEY = void 0;
-const common_1 = require("@nestjs/common");
-exports.MODULE_ACCESS_KEY = 'moduleAccess';
-const RequireModule = (module, minAccess) => (0, common_1.SetMetadata)(exports.MODULE_ACCESS_KEY, { module, minAccess });
-exports.RequireModule = RequireModule;
