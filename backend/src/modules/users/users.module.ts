086ba693a66b8acd93b61e5066e7bd6af308193f
Ubuntu <ubuntu@vps-3051a159.vps.ovh.ca>
2026-08-05 10:39:20 +0000

    fix: hacer UsersModule global para resolver ModuleAccessGuard en todos los modulos

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from '../common/prisma.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UserPermissionsService } from './user-permissions.service';
 
+@Global()
 @Module({
   controllers: [UsersController],
   providers: [UsersService, UserPermissionsService, PrismaService],
